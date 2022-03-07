3382e7b9fc83f06f4e72313acc0318f06f9f482a
Jerrod Long <me@jerrodlong.com>
2022-03-07 18:58:11 -0500

    [update] Remove nav links

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -50,9 +50,9 @@
                     <ul class="nav navbar-nav navbar-right">
                         <!-- Authentication Links -->
                         @if (Auth::guest())
-                            <li><a href="{{ url('/projects') }}">Projects</a></li>
+                            {{-- <li><a href="{{ url('/projects') }}">Projects</a></li>
                             <li><a href="{{ url('/changelog') }}">Changelog</a></li>
-                            <li><a href="{{ url('/contacts') }}">Contact</a></li>
+                            <li><a href="{{ url('/contacts') }}">Contact</a></li> --}}
                         @else
                             <li class="dropdown">
                                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
